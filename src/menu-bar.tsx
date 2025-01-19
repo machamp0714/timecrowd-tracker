97fe8ac1ba23fe43189becf85d1cc111c7d24c59
machamp0714 <machamp0714@gmail.com>
2025-01-19 11:19:22 +0900

    feat: add menu bar command to show current time entry

---
+++ src/menu-bar.tsx
@@ -0,0 +1,28 @@
+import { MenuBarExtra } from "@raycast/api";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import { useUser } from "@/hooks";
+import { useCurrentTime } from "@/hooks";
+dayjs.extend(duration);
+
+export default function Command() {
+  const currentTime = useCurrentTime();
+  const { isLoadingUser, user } = useUser();
+
+  const elapsedTime = dayjs.duration(currentTime.diff(dayjs(user?.time_entry.started_at))).format("HH:mm:ss");
+
+  return (
+    <MenuBarExtra
+      icon="command-icon.png"
+      title={user?.time_entry.task.title}
+      tooltip="Current Time Entry"
+      isLoading={isLoadingUser}
+    >
+      {user?.time_entry && (
+        <MenuBarExtra.Section title="Running Time Entry">
+          <MenuBarExtra.Item title={`${user.time_entry.task.title} ${elapsedTime}`} />
+        </MenuBarExtra.Section>
+      )}
+    </MenuBarExtra>
+  );
+}
